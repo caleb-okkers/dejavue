08a78a8249a5bc400e8c32b095194897bac3651e
caleb-okkers <okkerscaleb@gmail.com>
2024-06-24 15:30:42 +0200

    added json data

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CarsView from '../views/CarsView.vue'
+import ConsolesView from '../views/ConsolesView.vue'
+import FriendsView from '../views/FriendsView.vue'
 
 const routes = [
   {
@@ -14,7 +17,24 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/cars',
+    name: 'Cars',
+    component: CarsView
+  },
+  {
+    path: '/consoles',
+    name: 'Consoles',
+    component: ConsolesView
+  },
+  {
+    path: '/friends',
+    name: 'Friends',
+    component: FriendsView
   }
+  
+
 ]
 
 const router = createRouter({
